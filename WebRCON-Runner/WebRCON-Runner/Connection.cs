e81d78014096abdb035a498b18207a6a708119da
PsychoTea <bensparkes8@gmail.com>
2017-11-30 22:53:29 +0000

    Added websocket connection and sending of commands

---
+++ WebRCON-Runner/WebRCON-Runner/Connection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using WebSocketSharp;
+
+namespace WebRCON_Runner
+{
+    class Connection
+    {
+        private Action<string> outputFunction;
+        private Action connectedCallback;
+
+        private IPAddress ipAddress;
+        private int port;
+        private string password;
+
+        private WebSocket socket;
+
+        public Connection(Action<string> outputFunction, IPAddress ipAddress, int port, string password)
+        {
+            this.outputFunction = outputFunction;
+            this.ipAddress = ipAddress;
+            this.port = port;
+            this.password = password;
+        }
+
+        public void Connect(Action connectedCallback)
+        {
+            this.connectedCallback = connectedCallback;
+
+            string wsURL = $"ws://{ipAddress}:{port}/{password}";
+
+            socket = new WebSocket(wsURL)
+            {
+                WaitTime = TimeSpan.FromSeconds(5)
+            };
+
+            socket.OnOpen += Socket_OnOpen;
+            socket.OnMessage += Socket_OnMessage;
+            socket.OnClose += Socket_OnClose;
+            socket.OnError += Socket_OnError;
+
+            outputFunction.Invoke($"Connecting to {wsURL}...");
+
+            socket.Connect();
+        }
+
+        public void Disconnect()
+        {
+            socket?.Close();
+        }
+
+        public void SendCommand(string command)
+        {
+            Dictionary<string, object> packet = new Dictionary<string, object>()
+            {
+                { "Identifier", 1},
+                {"Message", command },
+                { "Name", "WebRCON-Runner" }
+            };
+
+            string contents = JsonConvert.SerializeObject(packet);
+
+            socket.Send(contents);
+
+            outputFunction.Invoke($"Sent command: {command}");
+        }
+
+        private void Socket_OnOpen(object sender, EventArgs e)
+        {
+            outputFunction.Invoke($"Connection opened!");
+
+            connectedCallback.Invoke();
+        }
+
+        private void Socket_OnClose(object sender, CloseEventArgs e)
+        {
+            outputFunction.Invoke($"Connection closed!");
+        }
+
+        private void Socket_OnMessage(object sender, MessageEventArgs e)
+        {
+            outputFunction.Invoke($"Message recieved:");
+            outputFunction.Invoke(e.Data);
+        }
+
+        private void Socket_OnError(object sender, ErrorEventArgs e)
+        {
+            outputFunction.Invoke($"Connection error: {e.Message}");
+            outputFunction.Invoke(e.Exception.ToString());
+        }
+    }
+}
--- WebRCON-Runner/WebRCON-Runner/Program.cs
+++ WebRCON-Runner/WebRCON-Runner/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace WebRCON_Runner
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Connection connection;
+
+        private static IPAddress ipAddress;
+        private static int port;
+        private static string password;
+        private static string command;
+
+        public static void Main(string[] args)
         {
+            if (args.Length < 4)
+            {
+                PrintUsage();
+                return;
+            }
+
+            if (!IPAddress.TryParse(args[0], out ipAddress))
+            {
+                PrintUsage();
+                return;
+            }
+
+            if (!Int32.TryParse(args[1], out port))
+            {
+                PrintUsage();
+                return;
+            }
+
+            password = args[2];
+
+            command = string.Join(" ", args.Skip(3).ToArray());
+            
+            connection = new Connection(x => ConnectionOutput(x), ipAddress, port, password);
+
+            connection.Connect(() => OnConnection());
+        }
+
+        private static void ConnectionOutput(string message)
+        {
+            Console.WriteLine($"[Connection] {message}");
+        }
+
+        public static void OnConnection()
+        {
+            connection.SendCommand(command);
+
+            Console.WriteLine($"Finished. Closing...");
+
+            // Wait one second to allow the server to send 
+            // back any responses to the command
+            Thread.Sleep(1000);
+
+            connection.Disconnect();
+        }
+
+        private static void PrintUsage()
+        {
+            string programName = AppDomain.CurrentDomain.FriendlyName;
+
+            Console.WriteLine($"Usage: {programName} {{ip}} {{port}} {{password}} {{command...}}");
         }
     }
 }
